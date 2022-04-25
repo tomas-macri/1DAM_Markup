a5e7d8e772c2208d4014c54cb4fd5d40f22e4813
tomas-macri <emacri@educa.madrid.org>
2022-04-25 15:06:58 +0200

    cosas raras de jsdom

--- jsSecondAssignmment/public_html/js/practica2.js
+++ jsSecondAssignmment/public_html/js/practica2.js
@@ -1,40 +1,26 @@
 const CODIGO=25;
-console.log(misterio8(`<!DOCTYPE html>
-          <html><head></head>
-                <body>
-                <h1 id="ejemploh1" class="verde" style="color:red">Este es un h1</h1>
-                <h1 class="rojo" style="color:red">Este es un h1 rojo</h1>
-                <h1 class="clase">primero</h1>
-                <h1 class="clase">segundo</h1>
-                <h1 class="clase">3</h1>
-                <h1 class="clase">4</h1>
-                <div class="clase">puede</div>
-                <p id="test" class="clase">ask</p>
-                <h2 id="unh2" class="clase">puede</h2>
-                </body>
-            </html>`));
 
-function misterio1(document,id){
-    if(document.getElementById(id)){
-        return document.getElementById(id).innerHTML;
+function misterio1(domDocument,id){
+    if(domDocument.getElementById(id)){
+        return domDocument.getElementById(id).innerHTML;
     }
     else{
         return "No existe ese id";
     }
 }
 
-function misterio2(doument, className){
+function misterio2(domDocument, className){
     let resultado = "No existen con esa clase";
-    let elements = document.getElementsByClassName(className);
+    let elements = domDocument.getElementsByClassName(className);
     if (elements.length > 0)
     {
-       resultado = document.getElementsByClassName(className)[0].style.color;
+       resultado = domDocument.getElementsByClassName(className)[0].style.color;
     }
     return resultado;
 }
-function misterio3(DOMDocument, tagName){
+function misterio3(domDocument, tagName){
     let resultado = "No existen con esa etiqueta";
-    let elements = document.getElementsByTagName(tagName);
+    let elements = domDocument.getElementsByTagName(tagName);
     if (elements.length > 0){
         for (let i = 0; i < elements.length; i++){
             elements[i].id = "el" + tagName + (i+1);
@@ -45,58 +31,80 @@ function misterio3(DOMDocument, tagName){
     }
     
 }
-function misterio4(){
-    return true;
+function misterio4(domDocument, tagName, existingClass){
+    if (domDocument.querySelector(tagName+"."+existingClass) !== null){
+        return true;
+    }
+    return false;
 }
-function misterio5(){
-    return true;
+
+function misterio5(domDocument, classN){
+    return domDocument.getElementsByClassName(classN).length;
 }
-function misterio6(DOMDocument, newID){
-   let element = document.createElement("h1");
+function misterio6(domDocument, newID){
+   let element = domDocument.createElement("h1");
    element.id = newID;
-   document.body.appendChild(element);
+   domDocument.body.appendChild(element);
    return true;
 }
-function misterio7(DOMDocument, newID, newText, parentID){
-   let element = document.createElement("h2");
+function misterio7(domDocument, newID, newText, parentID){
+   let element = domDocument.createElement("h2");
    element.id = newID;
-   let text = document.createTextNode(newText);
+   let text = domDocument.createTextNode(newText);
    element.appendChild(text);
-   let parent = document.getElementById(parentID);
+   let parent = domDocument.getElementById(parentID);
    if(parent !== null){
        parent.appendChild(element);
    }
    else{
-       document.body.appendChild(element);
+       domDocument.body.appendChild(element);
    }
    return parent;
 }
-function misterio8(DOMDocument){
-    let ulList = document.createElement("ul");
+function misterio8(domDocument){
+    let ulList = domDocument.createElement("ul");
     for (let i = 0; i < 3; i++){
-        let liItem = document.createElement("li");
+        let liItem = domDocument.createElement("li");
         liItem.className = "linea";
-        let liText = document.createTextNode("HOLA");
+        let liText = domDocument.createTextNode("HOLA");
         liItem.appendChild(liText);
         ulList.appendChild(liItem);
     }
-    document.body.appendChild(ulList);
+    domDocument.body.appendChild(ulList);
     return ulList;
 }
-function misterio9(){
-    return true;
+function misterio9(domDocument){
+    let liElements = domDocument.getElementsByClassName("linea");
+    let parent = domDocument.getElementsByTagName("ul")[0];
+    let newValues = Array("primero", "segundo", "tercero");
+    
+    for (let i = 0; i<liElements.length;i++){
+        let liItem = liElements[i];
+        liItem.id = newValues[i];
+        liItem.innerHTML = newValues[i];
+        let liText = domDocument.createTextNode(newValues[i]);
+        parent.replaceChild(liItem, liElements[i]);
+    }
 }
+
 function misterio10(){
     return true;
 }
-function misterio11(){
+function misterio11(domDocument, idReplace){
+    let element = domDocument.getElementById(idReplace);
+    if (element !== null){
+        element.className = "clase";
+    }
+    else{
+        return "No existe ese id";
+    }
     return true;
 }
 function misterio12(){
     return true;
 }
-function misterio13(){
-    return true;
+function misterio13(domDocument, newTitle){
+    domDocument.title = newTitle;
 }
 function misterio14(){
     return true;
@@ -119,4 +127,4 @@ function misterio14(){
     misterio13,
     misterio14,
     CODIGO
-}
+};
