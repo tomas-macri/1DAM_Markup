a151730339c2e2a65cb33f876ee1eba0692f40ec
tomas-macri <emacri@educa.madrid.org>
2022-05-11 13:41:57 +0200

    mas

--- simonSwipe/public_html/js/simonSwipe.js
+++ simonSwipe/public_html/js/simonSwipe.js
@@ -7,44 +7,47 @@ document.addEventListener("DOMContentLoaded", assignEvents);
 function assignEvents() {
     document.getElementById("btnCreateField").addEventListener("click", startGame);
 }
-
-function startGame() {
-
-    let myWidth = document.getElementById("columns").value;
-    let myHeight = document.getElementById("rows").value;
-
-    document.getElementById("rows").disabled = true;
-    document.getElementById("columns").disabled = true;
-    document.getElementById("level").disabled = true;
-    document.getElementById("btnCreateField").disabled = true;
-
-
-
-    document.getElementById("message").style.display = "block";
-
-    document.getElementById("crono").style.display = "block";
-    document.getElementById("score").style.display = "block";
-
-    drawField(myWidth, myHeight);
-}
-
-function drawField(myWidth, myHeight) {
-    let colorArray = ['#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399',
+    const colorArray = ['#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399',
         '#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680',
         '#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933',
         '#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3',
         '#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF'];
+    
+function startGame() {
+    let myWidth = document.getElementById("columns").value;
+    let myHeight = document.getElementById("rows").value;
+    let myLevel = document.getElementById("level").value;
 
+    disableBeginButtons();
+    displayBlockMessageAndCounters();   
+    drawField(myWidth, myHeight);
+    
+    generateSequence(myWidth, myHeight, myLevel);
+    
+}
+
+function generateSequence(x, y, level){
+    
+}
+
+function displayBlockMessageAndCounters(){
+    document.getElementById("message").style.display = "block";
+    document.getElementById("crono").style.display = "block";
+    document.getElementById("score").style.display = "block";
+}
+
+function disableBeginButtons(){
+    document.getElementById("rows").disabled = true;
+    document.getElementById("columns").disabled = true;
+    document.getElementById("level").disabled = true;
+    document.getElementById("btnCreateField").disabled = true;
+}
+
+function drawField(myWidth, myHeight) {
     for (i = 0; i < myHeight; i++) {
         for (j = 0; j < myWidth; j++) {
-            let keys = document.createElement("button");
-            let newId = j + "_" + i;
-
-            keys.id = newId;
-            keys.className = "btnField";
-            keys.name = "btnField";
-            keys.style.backgroundColor = colorArray[(i * myWidth) + j];
-
+            
+            let keys = createButton(i, myWidth, j);
             document.getElementById("field").appendChild(keys);
         }
         let space = document.createElement("br");
@@ -53,12 +56,22 @@ function drawField(myWidth, myHeight) {
     enableEventsField();
 }
 
+function createButtons(i, myWidth, j){
+    let keys = document.createElement("button");
+    let newId = j + "_" + i;
+
+    keys.id = newId;
+    keys.className = "btnField";
+    keys.name = "btnField";
+    keys.style.backgroundColor = colorArray[(i * myWidth) + j];
+    return keys;
+}
+
 function enableEventsField() {
     let fieldButtons = document.getElementsByName("btnField");
     for (i = 0; i < fieldButtons.length; i++) {
         fieldButtons[i].addEventListener("mousedown", check);
     }
-
 }
 
 function check(event) {
@@ -67,4 +80,6 @@ function check(event) {
     let text = "The button " + coordinates + " has been clicked";
     messageElement.innerHTML = text;
 }
+
+
 
